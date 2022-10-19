a7547cfeadcb2d57c772c4403324ae8460c30456
ilkaFOG <94058603+ilkaFOG@users.noreply.github.com>
2022-10-19 18:18:48 +0300

    Folders
    small changes in the folder location

---
+++ model/command.py
@@ -0,0 +1,17 @@
+from model.actions import Actions
+
+
+class Command:
+    def __init__(self, action:Actions, description:str):
+        self.__action = action
+        self.__description = description
+    
+    def __str__(self):
+        return f'Введите \'{self.__action.value}\'- для {self.__description}'
+
+    def to_string(self):
+        return f'Введите \'{self.__action.value}\'- для {self.__description}'
+
+    @property
+    def action(self):
+        return self.__action
---
+++ model/state.py
@@ -0,0 +1,1 @@
+import UI.ui as ui
